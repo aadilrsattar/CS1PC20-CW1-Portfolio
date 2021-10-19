7ccaa8d218eeed6da1fe24f94e0c79b13f896080
Aadil Sattar <sx004098@student.reading.ac.uk>
2021-10-19 23:58:38 +0100

    code to add two vectors of fixed size

---
+++ week3/vectors/test_vector_add.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include "vector.h"
+/** A simple test framework for vector library
+* we will be improving on this later
+*/
+int main(void){
+/** xvec and yvec will be inputs to our vector arithmetic routines
+* zvec will take the return value
+*/
+	int xvec[SIZ]={1,2,3};
+	int yvec[SIZ]={5,0,2};
+	int zvec[SIZ];
+	add_vectors(xvec,yvec,zvec);
+	/** We want to check each element of the returned vector
+	*/
+	assert(6==zvec[0]);
+	assert(2==zvec[1]);
+	assert(5==zvec[2]);
+	/** If the assert worked, there wasn't an error so return 0
+	*/
+	return 0;
+}
+
---
+++ week3/vectors/vector.h
@@ -0,0 +1,3 @@
+#define SIZ 3
+int add_vectors(int x[], int y[], int z[]);
+
